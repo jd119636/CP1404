9b399c5b5a262a59f68914bf52fbe5bfb02c0090
jd119636 <indra.kiiver@my.jcu.edu.au>
2025-10-13 12:42:35 +1000

    Prac 2

---
+++ Prac 2/temperature.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 - Practical
+Program for temperature conversion
+"""
+def teperature_to_celsius(temp):
+    return (temp - 32) * 5/9
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        temperature_to_celsius = input("Enter temperature in Celsius: ")
+        # celsius = float(input("Celsius: "))
+        # fahrenheit = celsius * 9.0 / 5 + 32
+        # print(f"Result: {fahrenheit:.2f} F")
+    elif choice == "F":
+        fahrenheit = float(input("Fahrenheit : "))
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print(f"Result: {celsius:.2f} C")
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
