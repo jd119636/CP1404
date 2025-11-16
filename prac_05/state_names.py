8263214be2826bf4e2957821452bea97c15ed4b5
jd119636 <indra.kiiver@my.jcu.edu.au>
2025-11-16 14:19:50 +1000

    guitars section done

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 state_names = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
@@ -26,9 +25,3 @@ while state_code != "":
     except KeyError:
         print("Invalid short state")
         state_code = input("Enter short state: ").upper()
-# while state_code != "":
-#     if state_code in state_names:
-#         print(state_code, "is", state_names[state_code])
-#     else:
-#         print("Invalid short state")
-#     state_code = input("Enter short state: ").upper()
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "Ford")
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    print(f"{my_car}")
+    limo = Car(100, "Limo"  )
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(f"{limo}")
 
 
 main()
+
